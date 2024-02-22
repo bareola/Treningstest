729601722485919dd4c007584292c8b8d92ba0b7
bareola <31635437+bareola@users.noreply.github.com>
2024-02-22 19:26:28 +0100

    making progress

--- app/src/main/java/com/example/mintreningsplayground/MainActivity.kt
+++ app/src/main/java/com/example/mintreningsplayground/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +17,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -25,6 +30,7 @@ import com.example.mintreningsplayground.data.Datasource
 import com.example.mintreningsplayground.data.StyrkeExercise
 import com.example.mintreningsplayground.ui.theme.MinTreningsplaygroundTheme
 import com.example.mintreningsplayground.ui.theme.Typography
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
 class MainActivity : ComponentActivity() {
@@ -46,12 +52,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ExerciseCard(exercise: StyrkeExercise) {
+    var expanded by remember {mutableStateOf(false)}
     Card(modifier = Modifier
-        .fillMaxWidth()) {
+        .fillMaxWidth()
+        .clickable(onClick = { expanded = !expanded }))
+    {
+        Row {
+            Text(text = exercise.name, style = Typography.headlineMedium)
+        }
+        if (expanded)
         Row {
             ExerciseIcon(exercise)
             Column (modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))) {
-                Text(text = exercise.name, style = Typography.bodyLarge)
+
                 Text(text = exercise.description, style = Typography.bodyLarge)
                 for (i in 1..exercise.numberOfSets) {
                     Switch(checked = false, onCheckedChange = {/*TODO*/ })
