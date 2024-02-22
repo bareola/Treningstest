564c724332771a6ff06534515315096b1dbeba95
bareola <31635437+bareola@users.noreply.github.com>
2024-02-22 21:10:21 +0100

    keep on keeping on

--- app/src/main/java/com/example/mintreningsplayground/MainActivity.kt
+++ app/src/main/java/com/example/mintreningsplayground/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -21,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -30,7 +34,6 @@ import com.example.mintreningsplayground.data.Datasource
 import com.example.mintreningsplayground.data.StyrkeExercise
 import com.example.mintreningsplayground.ui.theme.MinTreningsplaygroundTheme
 import com.example.mintreningsplayground.ui.theme.Typography
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
 class MainActivity : ComponentActivity() {
@@ -52,29 +55,52 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ExerciseCard(exercise: StyrkeExercise) {
-    var expanded by remember {mutableStateOf(false)}
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .clickable(onClick = { expanded = !expanded }))
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = { expanded = !expanded }
+            )
+    )
     {
-        Row {
-            Text(text = exercise.name, style = Typography.headlineMedium)
+        Row(horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(
+                text = exercise.name,
+                style = Typography.headlineMedium,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            )
+            Spacer(Modifier.weight(1f))
+            Image(
+                painter = painterResource(id = if (expanded) {R.drawable.collapse_icon} else {R.drawable.expand}),
+                contentDescription = null,
+                alignment = Alignment.CenterEnd,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .size(
+                        dimensionResource(id = R.dimen.expand_icon)
+                    )
+            )
         }
         if (expanded)
-        Row {
-            ExerciseIcon(exercise)
-            Column (modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))) {
+            Row {
+                ExerciseIcon(exercise)
+                Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_small))) {
 
-                Text(text = exercise.description, style = Typography.bodyLarge)
-                for (i in 1..exercise.numberOfSets) {
-                    Switch(checked = false, onCheckedChange = {/*TODO*/ })
-                    Text(
-                        text = stringResource(id = R.string.setcomplete, i),
-                        style = Typography.bodyLarge
-                    )
+                    Text(text = exercise.description, style = Typography.bodyLarge)
+                    for (i in 1..exercise.numberOfSets) {
+                        Row {
+                            Text(
+                                text = stringResource(id = R.string.setcomplete, i),
+                                style = Typography.bodyLarge, modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                            Spacer(Modifier.weight(1f))
+                            Switch(checked = false, onCheckedChange = {/*TODO*/ }, modifier = Modifier.align(Alignment.CenterVertically))
+                        }
+                    }
                 }
             }
-        }
     }
 }
 
@@ -83,7 +109,8 @@ fun ExerciseIcon(icon: StyrkeExercise, modifier: Modifier = Modifier) {
     Image(
         painterResource(id = icon.image),
         contentDescription = null,
-        modifier = modifier.size(dimensionResource(id = R.dimen.icon_size)
+        modifier = modifier.size(
+            dimensionResource(id = R.dimen.icon_size)
         )
     )
 }
